cff352663616d56a75e2cbc6e6d1760fae2f987c
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-03-21 16:59:56 -0300

    feat: add Score class

---
+++ web/scripts/lib/Score.js
@@ -0,0 +1,12 @@
+import { Text } from "./Text.js";
+
+export class Score extends Text {
+  constructor(textConfig) {
+    super(textConfig);
+  }
+
+  update(engineState) {
+    const score = 40 - (engineState.gameObjects.length - 3);
+    this.content = `Score : ${score}/40`;
+  }
+}
