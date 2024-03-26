c530cf5684f4fed929bea0491bfb0a07a0497f5c
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-03-26 15:23:38 -0300

    feat(breakout): improve score logic

--- web/js/games/breakout/Score.js
+++ web/js/games/breakout/Score.js
@@ -1,12 +1,24 @@
 import { Text } from "../../lib/Text.js";
 
 export class Score extends Text {
+  #maxPossibleScore;
+
   constructor(textConfig) {
     super(textConfig);
   }
 
   update(engineState) {
-    const score = 40 - (engineState.gameObjects.length - 3);
-    this.content = `Score : ${score}/40`;
+    if (!this.#maxPossibleScore) {
+      this.#maxPossibleScore = engineState.gameObjects.length - 3;
+    }
+
+    const score = this.#maxPossibleScore - (engineState.gameObjects.length - 3);
+
+    this.content = `Score : ${score}/${this.#maxPossibleScore}`;
+
+    if (score === this.#maxPossibleScore) {
+      engineState.stores["game-state"] = 0;
+      return;
+    }
   }
 }
